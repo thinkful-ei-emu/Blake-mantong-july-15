2c692eb5435c506f44cfeb24dec78f96962dad16
Blake Lowrey <blake.lowrey1@gmail.com>
2019-07-15 15:54:45 -0400

    exersize 2

--- index.js
+++ index.js
@@ -25,10 +25,38 @@ app.get('/sum',(req,res)=>{
   if(typeof floatB !=='number'){
     return res.status(400).send('B is not a number');
   }
-  const c = parseFloat(c);
+  const c = floatA + floatB;
   const sum = `The sum of ${a} and ${b} is ${c}`;
 
   res.send(sum);
+});
+
+app.get('/cipher',(req,res)=>{
+  const{text , shift} = req.query;
+  if(!text){
+    return res.status(400).send('Please provide a text');
+  }
+  if(!shift){
+    return res.status(400).send('Plese provide a shift');
+  }
+  let shiftInt = parseInt(shift);
+  if(typeof text !=='string'){
+    return res.status(400).send('text is not a string');
+  }
+  if(typeof shiftInt !=='number'){
+    return res.status(400).send('shift is not a number');
+  }
+
+  let wordArray = text.split('');
+  let resultArray = [];
+  wordArray.forEach(letter => {
+    let letterCode = letter.charCodeAt(0);
+    let newLetterCode = letterCode + shiftInt;
+    resultArray.push(String.fromCharCode(newLetterCode));
+  });
+
+  let result = resultArray.join('');
+  return res.send(result);
 });
  
 app.listen(8080, ()=> console.log('Server on 8080') );
